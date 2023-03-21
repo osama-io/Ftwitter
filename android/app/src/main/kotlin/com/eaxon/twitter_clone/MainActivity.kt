b599e121515423696499ad669a3b9c39d6c112e2
osama <osama.buic@gmail.com>
2023-03-21 19:05:37 +0500

    initail commet

---
+++ android/app/src/main/kotlin/com/eaxon/twitter_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eaxon.twitter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,7 @@
+class AppwriteConstants {
+  static const String databaseId = '63f2f7b19477f33738c5';
+  static const String projectId = '63f2edfeaec302c25250';
+  static const String endPoint = 'http://localhost:80/v1';
+
+
+}
